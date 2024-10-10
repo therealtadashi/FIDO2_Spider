557f69d78dff2143ea91ca1816c41343c2b63884
Tadashi Toyokawa <tadashi.toyokawa@hm.edu>
2024-10-10 17:35:44 +0200

    main: small changes

--- src/main.py
+++ src/main.py
@@ -1,3 +1,7 @@
 from src.modules.login_search import search_common_login_path_for_url
+from src.utils.csv_url_reader import read_url
+from src.utils.fido2_support_writer import update_fido2_support_json
 
-login_urls, support_urls = search_common_login_path_for_url()
+domain = read_url()[0]
+login_urls, support_urls = search_common_login_path_for_url(domain)
+update_fido2_support_json(domain, login_urls, support_urls)
